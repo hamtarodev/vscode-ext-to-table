5bf67ff3c66e282dd5c249013cc6010deb5de9a0
Hamtaro The Dev <jorolfo1119@gmail.com>
2022-02-05 16:23:44 +0800

    sample commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 
+// sample commit
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "ToTable" is now active!');
 	let disposable = vscode.commands.registerCommand('ToTable.viewTable', () => {
