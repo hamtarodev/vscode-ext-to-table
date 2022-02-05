9c660968e28ad8f8de0cda0b3e81f00586bbb1b0
John Eric <earljohn3ric@gmail.com>
2022-02-05 16:13:25 +0800

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,12 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "ToTable" is now active!');
+	let disposable = vscode.commands.registerCommand('ToTable.viewTable', () => {
+		vscode.window.showInformationMessage('Hello World from ToTable!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
